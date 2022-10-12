269cf6c568601a113fc41e2a520c8fea40e11dfa
SunilKumar-23 <115451713+SunilKumar-23@users.noreply.github.com>
2022-10-12 11:14:36 +0530

    my first project

---
+++ Basic.java
@@ -0,0 +1,9 @@
+class Demo{
+    public static void main(String [] args){
+        System.out.println("adding of two integers");
+        int a = 20;
+        int b = 30;
+        int sum = a+b;
+        System.out.println("The sum of two integers is : " + sum);
+    }
+}
